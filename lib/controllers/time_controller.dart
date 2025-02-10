346526d49cf00337747c26ff52c10f3b13678158
LirishaIgnis <drakorra@gmail.com>
2025-02-10 15:09:21 -0500

    Implementacion visual y coreccion de tiempo ok

--- lib/controllers/time_controller.dart
+++ lib/controllers/time_controller.dart
@@ -8,7 +8,8 @@ import '../controllers/game_controller.dart';
 class TimeController extends ChangeNotifier {
   final GameState _gameState;
   final BluetoothService _bluetoothService;
-  Timer? _timer;
+  Timer? _tramaTimer;
+  Timer? _relojTimer;
   bool _bitOscilacion = false; // Alterna entre 6 y 2
 
   TimeController(this._gameState, this._bluetoothService);
@@ -16,39 +17,50 @@ class TimeController extends ChangeNotifier {
   GameState get gameState => _gameState;
 
   void iniciarTiempo() {
-    if (_timer == null) {
-      _timer = Timer.periodic(Duration(milliseconds: 500), (timer) {
+    if (_tramaTimer == null && _relojTimer == null) {
+      // Timer para enviar la trama cada 500 ms
+      _tramaTimer = Timer.periodic(Duration(milliseconds: 500), (timer) {
+        _enviarTrama();
+      });
+
+      // Timer para actualizar el reloj cada 1 segundo
+      _relojTimer = Timer.periodic(Duration(seconds: 1), (timer) {
         _actualizarTiempo();
       });
     }
   }
 
   void pausarTiempo() {
-    _timer?.cancel();
-    _timer = null;
+    _tramaTimer?.cancel();
+    _tramaTimer = null;
+    _relojTimer?.cancel();
+    _relojTimer = null;
   }
 
   /// **Reiniciar pone en 0 el tiempo y los marcadores (sin afectar el periodo)**
   void reiniciarTiempo(GameController gameController) {
-    _timer?.cancel();
-    _timer = null;
+    _tramaTimer?.cancel();
+    _relojTimer?.cancel();
+    _tramaTimer = null;
+    _relojTimer = null;
     gameController.reiniciarMarcadoresYTiempo();
     notifyListeners();
   }
 
+  /// **Actualiza el tiempo del reloj cada 1 segundo**
   void _actualizarTiempo() {
-    _bitOscilacion = !_bitOscilacion; // Alternar entre 6 y 2
-
     if (_gameState.segundos == 59) {
       _gameState.minutos++;
       _gameState.segundos = 0;
     } else {
       _gameState.segundos++;
     }
-
     notifyListeners();
+  }
 
-    // Generar la trama correctamente codificada y enviarla como Uint8List
+  /// **Envía la trama cada 500 ms**
+  void _enviarTrama() {
+    _bitOscilacion = !_bitOscilacion; // Alternar entre 6 y 2
     Uint8List trama = _gameState.generarTramaEstadoPartido(_bitOscilacion ? 6 : 2);
     _bluetoothService.enviarTrama(trama);
   }
